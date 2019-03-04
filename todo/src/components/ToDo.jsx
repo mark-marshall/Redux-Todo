d5814bc9d3acb3f9cf1dedc9c1f8e10927e29da8
mark-marshall <mark.marshallgp@gmail.com>
2019-03-04 16:41:55 +0000

    Added complete line-through styles for completed:true todos

--- todo/src/components/ToDo.jsx
+++ todo/src/components/ToDo.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import styled from 'styled-components';
+
+const ToDoWrappers = styled.div`
+  .complete {
+    text-decoration: line-through;
+    color: red;
+  }
+`;
 
 const changeCompleted = toDoInfo => ({
   type: 'CHANGE_COMPLETED',
@@ -15,17 +23,22 @@ const deleteItem = toDoID => ({
 export function ToDo({ changeCompleted, deleteItem, todo }) {
   return (
     <div>
-      <li
-        onClick={() =>
-          changeCompleted({
-            id: todo.id,
-            completedStatus: !todo.completed,
-          })
-        }
-      >
-        {todo.value}
-      </li>
-      <button onClick={() => deleteItem(todo.id)} type="submit">Delete item</button>
+      <ToDoWrappers>
+        <li
+          onClick={() =>
+            changeCompleted({
+              id: todo.id,
+              completedStatus: !todo.completed,
+            })
+          }
+          className={todo.completed ? 'complete' : 'incomplete'}
+        >
+          {todo.value}
+        </li>
+      </ToDoWrappers>
+      <button onClick={() => deleteItem(todo.id)} type="submit">
+        Delete item
+      </button>
     </div>
   );
 }
