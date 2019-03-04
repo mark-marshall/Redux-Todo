74a1d3afbc58fed7419dda6d8819954c19618f3a
mark-marshall <mark.marshallgp@gmail.com>
2019-03-04 12:43:54 +0000

    [MVP] Removed temporary state from App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,13 +5,6 @@ import ToDoList from './components/ToDoList';
 import InputToDo from './components/InputToDo';
 
 class App extends Component {
-  // Setting up temporary state on App for fleshing out application components 
-  state = {
-    todos: [{
-      value: 'bake cakes',
-      completed:false
-    }]
-  }
   render() {
     return (
       <div className="App">
--- todo/src/components/InputToDo.jsx
+++ todo/src/components/InputToDo.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  // Setting up temporary state on App for fleshing out application components 
   state = {
     input: '',
-  }
+  };
 
   handleInput = event => {
-      this.setState({
-          input: event.target.value,
-      })
-  }
+    this.setState({
+      input: event.target.value,
+    });
+  };
 
   render() {
     return (
       <div>
-          <input placeholder="Add a todo" value={this.state.input} onChange={this.handleInput}></input>
-          <button>Add ToDo</button>
+        <input
+          placeholder="Add a todo"
+          value={this.state.input}
+          onChange={this.handleInput}
+        />
+        <button>Add ToDo</button>
       </div>
     );
   }
